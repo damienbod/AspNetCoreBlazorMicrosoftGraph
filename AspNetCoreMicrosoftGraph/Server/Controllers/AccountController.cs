54ae271da6fe938f19be0b641a7a48ef44db4d25
damienbod <damien_bod@hotmail.com>
2021-12-21 15:40:28 +0100

    Adding Anti-forgery explicitly

--- AspNetCoreMicrosoftGraph/Server/Controllers/AccountController.cs
+++ AspNetCoreMicrosoftGraph/Server/Controllers/AccountController.cs
@@ -19,7 +19,7 @@ namespace AspNetCoreMicrosoftGraph.Server.Controllers
             });
         }
 
-        // [ValidateAntiForgeryToken] // not needed explicitly due the the Auto global definition.
+        [ValidateAntiForgeryToken] // not needed explicitly due the the Auto global definition.
         [Authorize]
         [HttpPost("Logout")]
         public IActionResult Logout()
