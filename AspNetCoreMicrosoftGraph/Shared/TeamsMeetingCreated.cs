40ab1d674bb5dcea6a3799738bacb60d9e3122e3
damienbod <damien_bod@hotmail.com>
2022-01-14 21:40:34 +0100

    Init Teams meeting

---
+++ AspNetCoreMicrosoftGraph/Shared/TeamsMeetingCreated.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace AspNetCoreMicrosoftGraph
+{
+    public class TeamsMeetingCreated
+    {
+        public string JoinUrl { get; set; }
+
+        public string Subject { get; set; }
+
+        public List<string> Attendees { get; set; }
+
+        public string EmailSent { get; set; }
+    }
+}
---
+++ AspNetCoreMicrosoftGraph/Shared/TeamsMeetingDataModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreMicrosoftGraph
+{
+    public class TeamsMeetingDataModel
+    {
+        [Required]
+        public string Attendees { get; set; }
+        [Required]
+        public string MeetingName { get; set; }
+
+        [Required]
+        public DateTime? From { get; set; }
+
+        [Required]
+        public DateTime? To { get; set; }
+    }
+}
