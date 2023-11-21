738c4beb32f485877d7d96d141825aae9c066ff0
damienbod <damien_bod@hotmail.com>
2023-11-21 21:41:41 +0100

    Update Graph SDK 5

--- AspNetCoreMicrosoftGraph/Server/Services/Delegated/MicrosoftGraphDelegatedClient.cs
+++ AspNetCoreMicrosoftGraph/Server/Services/Delegated/MicrosoftGraphDelegatedClient.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Graph;
+using Microsoft.Graph.Me.SendMail;
+using Microsoft.Graph.Models;
 
 namespace AspNetCoreMicrosoftGraph.Server.Services;
 
@@ -11,103 +13,38 @@ public class MicrosoftGraphDelegatedClient
         _graphServiceClient = graphServiceClient;
     }
 
-    public async Task<User?> GetGraphApiUser(string? email)
-    {
-        if (email == null) return null;
-
-        var id = await GetUserIdAsync(email);
-        if (string.IsNullOrEmpty(id))
-            return null;
-
-        return await _graphServiceClient.Users[id]
-            .Request()
-            .GetAsync();
-    }
-
-    private async Task<string> GetUserIdAsync(string email)
-    {
-        var filter = $"userPrincipalName eq '{email}'";
-        //var filter = $"startswith(userPrincipalName,'{email}')";
-
-        var users = await _graphServiceClient.Users
-            .Request()
-            .Filter(filter)
-            .GetAsync();
-
-        if(users.CurrentPage.Count == 0)
-        {
-            return string.Empty;
-        }
-        return users.CurrentPage[0].Id;
-    }
-
-    public async Task<List<Presence>> GetPresenceforEmail(string email)
-    {
-        var cloudCommunicationPages = await GetPresenceAsync(email);
-
-        var allPresenceItems = new List<Presence>();
-
-        while (cloudCommunicationPages != null && cloudCommunicationPages.Count > 0)
-        {
-            foreach (var presence in cloudCommunicationPages)
-            {
-                allPresenceItems.Add(presence);
-            }
-
-            if (cloudCommunicationPages.NextPageRequest == null)
-                break;
-        }
-
-        return allPresenceItems;
-    }
-
-    private async Task<ICloudCommunicationsGetPresencesByUserIdCollectionPage> GetPresenceAsync(string email)
-    {
-        var id = await GetUserIdAsync(email);
-
-        var ids = new List<string>()
-        {
-            id
-        };
-
-        return await _graphServiceClient.Communications
-            .GetPresencesByUserId(ids)
-            .Request()
-            .PostAsync();
-    }
-
     public async Task SendEmailAsync(Message message)
     {
         var saveToSentItems = true;
 
-        await _graphServiceClient.Me
-            .SendMail(message, saveToSentItems)
-            .Request()
-            .PostAsync();
+        var body = new SendMailPostRequestBody
+        {
+            Message = message,
+            SaveToSentItems = saveToSentItems
+        };
+
+        await _graphServiceClient.Me.SendMail
+            .PostAsync(body);
     }
 
-    public async Task<OnlineMeeting> CreateOnlineMeeting(OnlineMeeting onlineMeeting)
+    public async Task<OnlineMeeting?> CreateOnlineMeeting(OnlineMeeting onlineMeeting)
     {
         return await _graphServiceClient.Me
             .OnlineMeetings
-            .Request()
-            .AddAsync(onlineMeeting);
+            .PostAsync(onlineMeeting);
     }
 
-    public async Task<OnlineMeeting> UpdateOnlineMeeting(OnlineMeeting onlineMeeting)
+    public async Task<OnlineMeeting?> UpdateOnlineMeeting(OnlineMeeting onlineMeeting)
     {
         return await _graphServiceClient.Me
             .OnlineMeetings[onlineMeeting.Id]
-            .Request()
-            .UpdateAsync(onlineMeeting);
+            .PatchAsync(onlineMeeting);
     }
 
-    public async Task<OnlineMeeting> GetOnlineMeeting(string onlineMeetingId)
+    public async Task<OnlineMeeting?> GetOnlineMeeting(string onlineMeetingId)
     {
         return await _graphServiceClient.Me
             .OnlineMeetings[onlineMeetingId]
-            .Request()
             .GetAsync();
     }
 }
-
