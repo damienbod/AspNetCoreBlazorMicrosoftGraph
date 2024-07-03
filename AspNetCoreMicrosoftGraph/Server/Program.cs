80201a251e7568e0d11eeb4260202783aacbb030
damienbod <damien_bod@hotmail.com>
2024-07-03 16:36:28 +0200

    Improve startup

--- AspNetCoreMicrosoftGraph/Server/Program.cs
+++ AspNetCoreMicrosoftGraph/Server/Program.cs
@@ -1,18 +1,75 @@
-﻿namespace AspNetCoreMicrosoftGraph.Server;
+using AspNetCoreMicrosoftGraph.Server;
+using AspNetCoreMicrosoftGraph.Server.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+services.AddScoped<MicrosoftGraphDelegatedClient>();
+services.AddScoped<EmailService>();
+services.AddScoped<TeamsService>();
+
+services.AddScoped<MicrosoftGraphApplicationClient>();
+
+services.AddAntiforgery(options =>
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+    options.HeaderName = "X-XSRF-TOKEN";
+    options.Cookie.Name = "__Host-X-XSRF-TOKEN";
+    options.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Strict;
+    options.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.Always;
+});
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-         Host.CreateDefaultBuilder(args)
-             .ConfigureWebHostDefaults(webBuilder =>
-             {
-                 webBuilder
-                     .ConfigureKestrel(options => options.AddServerHeader = false)
-                     .UseStartup<Startup>();
-             });
+services.AddHttpClient();
+services.AddOptions();
+
+var scopes = builder.Configuration.GetValue<string>("DownstreamApi:Scopes");
+string[]? initialScopes = scopes?.Split(' ');
+if (scopes == null) throw new ArgumentNullException(nameof(scopes));
+
+services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration)
+    .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
+    .AddMicrosoftGraph("https://graph.microsoft.com/v1.0", initialScopes)
+    .AddInMemoryTokenCaches();
+
+services.AddControllersWithViews(options =>
+    options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute()));
+
+services.AddRazorPages().AddMvcOptions(options =>
+{
+    //var policy = new AuthorizationPolicyBuilder()
+    //    .RequireAuthenticatedUser()
+    //    .Build();
+    //options.Filters.Add(new AuthorizeFilter(policy));
+}).AddMicrosoftIdentityUI();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseWebAssemblyDebugging();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseSecurityHeaders(
+    SecurityHeadersDefinitions.GetHeaderPolicyCollection(app.Environment.IsDevelopment(),
+        app.Configuration["AzureAd:Instance"]));
+
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
